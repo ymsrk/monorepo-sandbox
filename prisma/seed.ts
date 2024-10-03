717347e9e9f28105b1c1e0662183f022eb4b93fd
山城 宏二郎 <kojiro.yamashiro@glv.co.jp>
2024-10-03 16:41:09 +0900

    💚 biomeの対象にしたため fix format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,6 @@
-import bcrypt from 'bcrypt';
-import prisma from '../libs/src/prismaClient';
-
-const nodeStatus = process.env.NODE_ENV
+import bcrypt from "bcrypt";
+import prisma from "../libs/src/prismaClient";
+const nodeStatus = process.env.NODE_ENV;
 
 async function main() {
   // パスワードのハッシュ化
@@ -10,63 +9,63 @@ async function main() {
   // サンプルデータの登録
   const user = await prisma.user.create({
     data: {
-      email: 'test@example.com',
-      name: 'Test User',
+      email: "test@example.com",
+      name: "Test User",
       password: hashedPassword,
       Post: {
         create: [
           {
-            title: 'First Post',
-            content: 'This is my first post.',
+            title: "First Post",
+            content: "This is my first post.",
           },
           {
-            title: 'Second Post',
-            content: 'This is my second post.',
-          }
-        ]
-      }
-    }
+            title: "Second Post",
+            content: "This is my second post.",
+          },
+        ],
+      },
+    },
   });
 
   const comment = await prisma.comment.create({
     data: {
-      content: 'This is a comment',
+      content: "This is a comment",
       author: {
-        connect: { id: user.id }
+        connect: { id: user.id },
       },
       post: {
-        connect: { id: 1 } // Example post ID
-      }
-    }
+        connect: { id: 1 }, // Example post ID
+      },
+    },
   });
-  
+
   // post_dateに新規登録
   const postDate = await prisma.postDate.create({
     data: {
       postCreatedAt: new Date(),
       postUpdatedAt: new Date(),
-    }
+    },
   });
-  
+
   const postDate2 = await prisma.postDate.create({
     data: {
       postCreatedAt: new Date(),
       postUpdatedAt: new Date(),
-    }
+    },
   });
-  
+
   const postDate3 = await prisma.postDate.create({
     data: {
       postCreatedAt: new Date(),
       postUpdatedAt: new Date(),
-    }
+    },
   });
 
-  console.log(`😻  ${nodeStatus}`)
+  console.log(`😻  ${nodeStatus}`);
   console.log({ user, comment, postDate });
-  
+
   const getPostDate = await prisma.postDate.findMany();
-  console.log(getPostDate[0].postUpdatedAt,);
+  console.log(getPostDate[0].postUpdatedAt);
 }
 
 main()
